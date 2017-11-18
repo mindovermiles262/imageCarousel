94ab616f71265fe01f1a7c553678370cbdfde82e
Andy Duss <mindovermiles262@gmail.com>
2017-11-17 22:56:06 -0800

    working

--- js/script.js
+++ js/script.js
@@ -5,27 +5,32 @@ let currentImage = 1
 
 function newImg(direction) {
   if (direction === "next") {
-    currentImage += 1
-    if (currentImage > nImages) {
-      currentImage = 1
-    }
+    (currentImage >= nImages) ? (currentImage = 1) : (currentImage += 1)
   } else if (direction === "prev") {
-    currentImage -= 1
-    if (currentImage == 0) {
-      currentImage = nImages
-    }
+    (currentImage == 1) ? (currentImage = nImages) : ( currentImage -= 1)
   }
   const imgTag = document.getElementsByTagName("img")[0]
   imgTag.src = "img/" + currentImage + ".jpg"
+  imgTag.className = ""
 }
 
 nextBtn.addEventListener("click", function() {
-  const img = document.getElementsByTagName("img")[0]
-  console.log(img)
-  newImg("next")
+  document.getElementsByTagName("img")[0].className += "slideRight"
+  setTimeout(function() {
+    newImg("next")
+  }, 150)
 })
 
 prevBtn.addEventListener("click", function() {
-  newImg("prev")
+  document.getElementsByTagName("img")[0].className += "slideLeft"  
+  setTimeout(function() {
+    newImg("prev")
+  }, 150)
 })
 
+setInterval(function() {
+  document.getElementsByTagName("img")[0].className += "slideRight"  
+  setTimeout(function() {
+    newImg("next")
+  }, 150)
+}, 5000)
