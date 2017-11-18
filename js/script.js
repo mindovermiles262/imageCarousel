6087e0b56399cb4aea5858ac5eb1f10d9f9ad756
Andy Duss <mindovermiles262@gmail.com>
2017-11-18 14:58:30 -0800

    refactor

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-const nextBtn = document.getElementById("next")
-const prevBtn = document.getElementById("prev")
-const nImages = 4
-let currentImage = 1
+const image = document.getElementById("currentImage");
+let autoSlide = startAutoSlide();
 
-function newImg(direction) {
-  if (direction === "next") {
-    (currentImage >= nImages) ? (currentImage = 1) : (currentImage += 1)
-  } else if (direction === "prev") {
-    (currentImage == 1) ? (currentImage = nImages) : ( currentImage -= 1)
-  }
-  const imgTag = document.getElementsByTagName("img")[0]
-  imgTag.src = "img/" + currentImage + ".jpg"
-  imgTag.className = ""
+Array.from(document.getElementsByTagName("button"))
+  .forEach(function (button) {
+    button.addEventListener("click", function () {
+      const thisButton = this;
+      clearInterval(autoSlide);
+      slide(thisButton.id);
+      autoSlide = startAutoSlide();
+    });
+  });
+
+function startAutoSlide() {
+  return setInterval(function () {
+    slide("right");
+  }, 5000);
 }
 
-nextBtn.addEventListener("click", function() {
-  document.getElementsByTagName("img")[0].className += "slideRight"
-  setTimeout(function() {
-    newImg("next")
-  }, 150)
-})
+function slide(direction) {
+  image.className = `slide${direction}`;
+  setTimeout(function () {
+    newImage(direction);
+  }, 150);
+}
 
-prevBtn.addEventListener("click", function() {
-  document.getElementsByTagName("img")[0].className += "slideLeft"  
-  setTimeout(function() {
-    newImg("prev")
-  }, 150)
-})
-
-setInterval(function() {
-  document.getElementsByTagName("img")[0].className += "slideRight"  
-  setTimeout(function() {
-    newImg("next")
-  }, 150)
-}, 5000)
+function newImage(direction) {
+  const numberOfImages = 4;
+  let currentImage = +image.src.match(/(\d+)(?!.*\d)/)[0];
+  if (direction === "right") {
+    currentImage = (currentImage >= numberOfImages) ? 1 : currentImage + 1;
+  }
+  else if (direction === "left") {
+    currentImage = (currentImage === 1) ? numberOfImages : currentImage - 1;
+  }
+  image.src = `img/${currentImage}.jpg`;
+  image.className = "";
+}
